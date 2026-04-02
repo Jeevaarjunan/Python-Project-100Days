f9ab3a08dc7662d6e082982bd291388b145f9124
Jeeva Arjunan <155565110+Jeevaarjunan@users.noreply.github.com>
2026-04-02 17:48:11 +0530

    Add files via upload
    Uploaded python scripts

---
+++ Band name generator(Basic).py
@@ -0,0 +1,5 @@
+
+print("Welcome to the Band generator")
+city_place = input("Can you provide the name of your city?\n")
+pet_name = input("What is your pet name?\n")
+print("Your band name can be " + city_place + " " + pet_name)
---
+++ Trip Calculator(Basic).py
@@ -0,0 +1,10 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+total_bill = bill + bill * tip / 100
+total_bill_amount = total_bill / people
+round_total_amount = round(total_bill_amount, 2)
+print(f"Each person should pay: ${round_total_amount}")
+
+
